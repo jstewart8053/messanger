b45176414cfd1a6d16483d9846896997301ab413
Jennifer Stewart <jstewart8053@gmail.com>
2020-09-26 21:14:28 -0500

    got rid of junk files

---
+++ messenger/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+
+
+
+  // setting name to pass below as props
+  // then in Tweets, call props.me to get name
+
+
+  return (
+    <div className="app">
+      <h1> Hello React </h1>
+
+    </div>
+  );
+}
+
+export default App;
