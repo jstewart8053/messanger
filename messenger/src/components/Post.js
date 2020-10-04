a6740a51bb96c744e18cb2df17c885897727b501
Jennifer Stewart <jstewart8053@gmail.com>
2020-10-04 03:53:22 -0500

    added firebase

--- messenger/src/components/Post.js
+++ messenger/src/components/Post.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import "../Post.css";
 import { Avatar } from "@material-ui/core";
 import VerifiedUserIcon from "@material-ui/icons/VerifiedUser";
@@ -7,46 +7,49 @@ import RepeatIcon from "@material-ui/icons/Repeat";
 import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
 import PublishIcon from "@material-ui/icons/Publish";
 
-function Post({
-    displayName,
-    username,
-    verified,
-    text,
-    image,
-    avatar
-}) {
-    return (
-        <div className='posts'>
-            <div className='post_avatar'>
-                <Avatar src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS0d4CrFaUnD7NjPTdmbULFdsZ9DCcifMoj4Q&usqp=CAU" />
-            </div>
-            <div className="post_body">
-                <div className="post_header">
-                    <div className="post_headerText">
-                        <h3>
-                            Dakota Johnson
-                             <span className="post_headerSpecial">
-                                <VerifiedUserIcon className='post_badge' />
-                            </span>
-                        </h3>
-                    </div>
-                    <div className='post_headerDescription'>
-                        <p> What a beautiful night sky! </p>
-                    </div>
+const Post = forwardRef(
+    ({ displayName,
+        username,
+        verified,
+        text,
+        image,
+        avatar
+    }, ref) => {
+        return (
+            <div className='post' ref={ref}>
+                <div className='post_avatar'>
+                    {/* <Avatar src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS0d4CrFaUnD7NjPTdmbULFdsZ9DCcifMoj4Q&usqp=CAU" /> */}
+                    <Avatar src={avatar} />
                 </div>
-                <img
-                    src="https://www.pnas.org/content/117/3/1240/F1.large.jpg"
-                    alt="image of a galaxy" />
-                <div className="post_footer">
-                    <ChatBubbleOutlineIcon fontSize='small' />
-                    <RepeatIcon fontSize='small' />
-                    <FavoriteBorderIcon fontSize="small" />
-                    <PublishIcon fontSize="small" />
-                </div>
+                <div className="post_body">
+                    <div className="post_header">
+                        <div className="post_headerText">
+                            <h3>
+                                {displayName}{""}
+                                <span className="post__headerSpecial">
+                                    {verified && <VerifiedUserIcon className="post__badge" />}
+                                 @ {username}
+                                </span>
+                            </h3>
+                        </div>
+                        <div className='post_headerDescription'>
+                            <p> {text} </p>
+                        </div>
+                    </div>
+                    <img
+                        src={image}
+                        alt="" />
+                    <div className="post_footer">
+                        <ChatBubbleOutlineIcon fontSize='small' />
+                        <RepeatIcon fontSize='small' />
+                        <FavoriteBorderIcon fontSize="small" />
+                        <PublishIcon fontSize="small" />
+                    </div>
 
+                </div>
             </div>
-        </div>
-    )
-}
+        )
+    }
+)
 
 export default Post;
